693f01fabebefce6367543d00ee9618c3df8bcbd
VixNH <48919848+VixNH@users.noreply.github.com>
2019-07-05 14:01:07 +0700

    Add files via upload

---
+++ Test/Test/QuanLyTienCong.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Test
+{
+    partial class QuanLyTienCong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button_xoa = new System.Windows.Forms.Button();
+            this.button_Sua = new System.Windows.Forms.Button();
+            this.button_them = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvTiencong = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTiencong)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button_xoa);
+            this.panel1.Controls.Add(this.button_Sua);
+            this.panel1.Controls.Add(this.button_them);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(14, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(695, 128);
+            this.panel1.TabIndex = 0;
+            // 
+            // button_xoa
+            // 
+            this.button_xoa.Location = new System.Drawing.Point(588, 31);
+            this.button_xoa.Name = "button_xoa";
+            this.button_xoa.Size = new System.Drawing.Size(92, 54);
+            this.button_xoa.TabIndex = 8;
+            this.button_xoa.Text = "Xóa";
+            this.button_xoa.UseVisualStyleBackColor = true;
+            this.button_xoa.Click += new System.EventHandler(this.button_xoa_Click);
+            // 
+            // button_Sua
+            // 
+            this.button_Sua.Location = new System.Drawing.Point(458, 29);
+            this.button_Sua.Name = "button_Sua";
+            this.button_Sua.Size = new System.Drawing.Size(97, 56);
+            this.button_Sua.TabIndex = 7;
+            this.button_Sua.Text = "Xem thông tin";
+            this.button_Sua.UseVisualStyleBackColor = true;
+            this.button_Sua.Click += new System.EventHandler(this.button_Sua_Click);
+            // 
+            // button_them
+            // 
+            this.button_them.Location = new System.Drawing.Point(320, 30);
+            this.button_them.Name = "button_them";
+            this.button_them.Size = new System.Drawing.Size(101, 55);
+            this.button_them.TabIndex = 6;
+            this.button_them.Text = "Thêm";
+            this.button_them.UseVisualStyleBackColor = true;
+            this.button_them.Click += new System.EventHandler(this.button_them_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(136, 85);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(136, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(137, 50);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(135, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(137, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(135, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tiền công:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên dịch vụ:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã loại dịch vụ:";
+            // 
+            // dgvTiencong
+            // 
+            this.dgvTiencong.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTiencong.Location = new System.Drawing.Point(21, 155);
+            this.dgvTiencong.Name = "dgvTiencong";
+            this.dgvTiencong.RowTemplate.Height = 24;
+            this.dgvTiencong.Size = new System.Drawing.Size(672, 210);
+            this.dgvTiencong.TabIndex = 1;
+            // 
+            // QuanLyTienCong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 371);
+            this.Controls.Add(this.dgvTiencong);
+            this.Controls.Add(this.panel1);
+            this.Name = "QuanLyTienCong";
+            this.Text = "Quản lý tiền công";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTiencong)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button_xoa;
+        private System.Windows.Forms.Button button_Sua;
+        private System.Windows.Forms.Button button_them;
+        private System.Windows.Forms.DataGridView dgvTiencong;
+    }
+}
---
+++ Test/Test/QuanLyTienCong.cs
@@ -0,0 +1,124 @@
+﻿using GetTienCongDTO;
+using LoaiTienCongBUS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class QuanLyTienCong : Form
+    {
+        public QuanLyTienCong()
+        {
+            InitializeComponent();
+        }
+        private TienCongBUS tcBus;
+        private void loadData_Vao_GridView()
+        {
+            tcBus = new TienCongBUS();
+            List<TienCongDTO> listTienCong = tcBus.select();
+
+            if (listTienCong == null)
+            {
+                MessageBox.Show("Có lỗi khi lấy thông tin");
+                return;
+            }
+            dgvTiencong.Columns.Clear();
+            dgvTiencong.DataSource = null;
+
+            dgvTiencong.AutoGenerateColumns = false;
+            dgvTiencong.AllowUserToAddRows = false;
+            dgvTiencong.DataSource = listTienCong;
+
+            DataGridViewTextBoxColumn clmatiencong = new DataGridViewTextBoxColumn();
+            clmatiencong.Name = "matiencong";
+            clmatiencong.HeaderText = "Mã tiền công";
+            clmatiencong.DataPropertyName = "matiencong";
+            clmatiencong.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dgvTiencong.Columns.Add(clmatiencong);
+
+            DataGridViewTextBoxColumn clloaidichvu = new DataGridViewTextBoxColumn();
+            clloaidichvu.Name = "tendichvu";
+            clloaidichvu.HeaderText = "Tên dịch vụ";
+            clloaidichvu.DataPropertyName = "tendichvu";
+            clloaidichvu.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dgvTiencong.Columns.Add(clloaidichvu);
+
+            DataGridViewTextBoxColumn cltiencong = new DataGridViewTextBoxColumn();
+            cltiencong.Name = "tiencong";
+            cltiencong.HeaderText = "Tiền công";
+            cltiencong.DataPropertyName = "tiencong";
+            cltiencong.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dgvTiencong.Columns.Add(cltiencong);
+
+            
+
+            CurrencyManager myCurrencyManager = (CurrencyManager)this.BindingContext[dgvTiencong.DataSource];
+            myCurrencyManager.Refresh();
+
+        }
+
+        private void button_them_Click(object sender, EventArgs e)
+        {
+            
+            tcBus = new TienCongBUS();
+            TienCongDTO tc = new TienCongDTO();
+            tc.MaTienCong = int.Parse(textBox1.Text);
+            tc.TenDichVu = textBox2.Text;
+            tc.TienCong = int.Parse(textBox3.Text);
+            bool kq = tcBus.them(tc);
+            if (kq == false)
+                MessageBox.Show("Thêm thông tin thất bại. Vui lòng kiểm tra lại dữ liệu");
+            else
+                MessageBox.Show("Thêm thông tin thành công");
+            this.loadData_Vao_GridView();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+        public void QuanLyTienCong_Load(object sender,EventArgs e)
+        {
+            tcBus = new TienCongBUS();
+        }
+        private void button_Sua_Click(object sender, EventArgs e)
+        {
+            this.loadData_Vao_GridView();
+        }
+
+        private void button_xoa_Click(object sender, EventArgs e)
+        {
+
+
+            tcBus = new TienCongBUS();
+            DialogResult = MessageBox.Show("Bạn có chắc chắn muốn xóa ?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (DialogResult == DialogResult.Yes)
+            {
+          
+                TienCongDTO tc = new TienCongDTO();
+
+                tc.MaTienCong = int.Parse(textBox1.Text);
+                //tc.TenDichVu = textBox2.Text;
+                //tc.TienCong = float.Parse(textBox3.Text);
+
+                bool kq = tcBus.xoa(tc);
+                if (kq == false)
+                    MessageBox.Show("Xóa thông tin thất bại. Vui lòng kiểm tra lại dữ liệu");
+                else
+                    MessageBox.Show("Xóa thông tin thành công");
+                this.loadData_Vao_GridView();
+                textBox1.Text = "";
+                //textBox2.Text = "";
+                //textBox3.Text = "";
+             
+
+            }
+
+        }
+    }
+}
