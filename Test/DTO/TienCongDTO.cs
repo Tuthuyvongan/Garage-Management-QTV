ff08d7e3427bf52c3f7cfd0e4f08396478df1296
VixNH <48919848+VixNH@users.noreply.github.com>
2019-07-05 14:00:37 +0700

    Add files via upload

---
+++ Test/DTO/TienCongDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetTienCongDTO
+{
+    public class TienCongDTO
+    {
+        private int maTienCong;
+        private string tenDichVu;
+        private float tienCong;
+
+
+        public int MaTienCong { get => maTienCong; set => maTienCong = value; }
+        public string TenDichVu { get => tenDichVu; set => tenDichVu = value; }
+        public float TienCong { get => tienCong; set => tienCong = value; }
+    }
+}
