9d65f10e3463b5ba2307d0f06e5cd1f0a74879ce
VixNH <48919848+VixNH@users.noreply.github.com>
2019-07-06 00:11:20 +0700

    Add files via upload

---
+++ Test/ClassLibrary1/NguoiDungBUS.cs
@@ -0,0 +1,42 @@
+﻿using nguoidungDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using nguoidungDAO;
+
+namespace nguoidungBUS
+{
+
+    public class NguoiDungBUS
+    {
+        private NguoiDungDAO tnDAO;
+        public NguoiDungBUS()
+        {
+            tnDAO = new NguoiDungDAO();
+        }
+        public bool them(NguoiDungDTO tn)
+        {
+            bool re = tnDAO.them(tn);
+            return re;
+        }
+
+        public bool sua(NguoiDungDTO tn)
+        {
+            bool re = tnDAO.sua(tn);
+            return re;
+        }
+
+        //public List<NguoiDungDTO> select(string tendangnhap,string password)
+        //    {
+        //        return tnDAO.select(tendangnhap,password);
+        //    }
+        //}
+        public bool kiemtra(NguoiDungDTO tn, string tendangnhap, string password)
+        {
+            bool re = tnDAO.kiemtra(tn, tendangnhap, password);
+            return re;
+        }
+    }
+}
