8e0dc3584e7403022e11973e4c74c0b6fac6730f
VixNH <48919848+VixNH@users.noreply.github.com>
2019-07-05 12:39:32 +0700

    Add files via upload

---
+++ Test/Test/MainMenu.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLGR
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void đăngXuấtToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LapPhieuSuaChua f =new LapPhieuSuaChua();
+            this.Hide();
+            f.ShowDialog();
+            this.Show();
+        }
+
+        private void thôngToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ThongTinNguoiDung f = new ThongTinNguoiDung();         
+            f.ShowDialog();           
+        }
+    }
+}
